6b4a8395c94e51bb09a659d3a71994fe6ef5ce6a
JackHCC <2508074836@qq.com>
2021-07-21 18:38:36 +0800

    Table init

--- src/com/jackcc/util/TypeConversion.java
+++ src/com/jackcc/util/TypeConversion.java
@@ -26,4 +26,17 @@ public class TypeConversion<T, V> {
 		return valueList;
 	}
 
+	public HashMap<T, V> list2hashMap(ArrayList<T> list1, ArrayList<V> list2) {
+		HashMap<T, V> hashMap;
+		if (list1.size() != list2.size()) {
+			return null;
+		} else {
+			hashMap = new HashMap<>();
+			for (int i = 0; i < list1.size(); i++) {
+				hashMap.put(list1.get(i), list2.get(i));
+			}
+		}
+		return hashMap;
+	}
+
 }
